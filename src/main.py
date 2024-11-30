eda46f0c8fdb6cc3e2253fc276a68c8da327c8ae
Vaibhav Pratap Singh <v8v88v8v88@protonmail.com>
2024-11-30 23:54:18 +0530

    Added basic FileSystem class with folder creation

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print('Basic File System Emulator')
+
+import os
+
+class FileSystem:
+    def __init__(self):
+        self.current_directory = os.getcwd()
+
+    def create_folder(self, folder_name):
+        os.makedirs(folder_name, exist_ok=True)
+
